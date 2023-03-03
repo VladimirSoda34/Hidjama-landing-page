5b7e0b91794a42ce9315fecc3755ae92dc2616d5
novoexpert <novoexpert@gmail.com>
2023-03-03 12:16:55 +0300

    changet

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,22 @@
+const submit = document.getElementById("submit")
 
-
-const animItems = document.getElementsByClassName('_anim-items');
-console.log(animItems);
-
-
-if (animItems.length > 0) {
-window. addEventListener('scroll', animOnScroll);
-function animOnScroll() {
-for (let index = 0; index < animItems.length; index++) {
-    
-    const animItem = animItems[index];
-    const animItemHeight = animItem.offsetHeight;
-    const animItemOffset = offset(animItem).top;
-    const animStart = 4;
-    
-    let animItemPoint = window.innerHeight - animItemHeight / animStart;
-    if (animItemHeight > window.innerHeight) {
-        animItemPoint = window.innerHeight - window.innerHeight / animStart;
-    }
-    
-
-if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
-animItem.classList.add('_active');
-
-} else { animItem.classList.remove('_active');
-
-}
-}
+const msgSent = () => {
+  alert("Данные успешно отправлены, ожидайте обратной связи!")
 }
 
-function offset(el) {
+submit.addEventListener("click", msgSent)
 
-const rect = el.getBoundingClientRect(),
-scrollLeft = window.scrollX || document.documentElement.scrollLeft,
-scrollTop = window.scrollY || document.documentElement.scrollTop;
 
-return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
-}
-setTimeout(animOnScroll, 1000)
+const modalLogo = document.getElementById("modal__logo")
 
-}
+
+
+modalLogo.addEventListener("click", () => {
+
+  modalLogo.src = "./images/logo/logo-black.png"
+  setTimeout(()=>{
+    modalLogo.src = "./images/logo/logo-white.png"
+  }, 2000)
+})
+
+
